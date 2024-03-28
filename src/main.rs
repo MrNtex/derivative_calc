f1aec82be2b046c8062632f9d4b2c1259d9f9579
MrNtex <ntexyt@gmail.com>
2024-03-28 18:20:55 +0100

    Add split_expression and calculate_simple_derivative functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq)]
 enum Operator {
     Pow,
     Sin,
@@ -27,92 +28,135 @@ fn main() {
             std::process::exit(1);
         }
         equasion = args[2].clone();
-        println!("{}", simple_expression(2, Operator::Exp, "x".to_string(), None));
+        let mut result = split_expression(equasion.clone());
+
+        for i in 0..result.len() {
+            println!("{}", result[i].clone());
+            println!("{}", calculate_simple_derivative(result[i].clone()));
+        }
     }
 
     if equasion.is_empty() {
         std::process::exit(1);
     }
 }
-// fn calculate_derivative(eq: String){
-//     let mut derivative = String::new();
 
-//     let mut brackets = false;
-
-//     let mut buffor = String::new();
-//     let mut numberBuffor = String::new();
-
-//     let mut levelOfBrackets = 0;
-//     for c in eq.chars() {
-//         if brackets {
-//             if c == ')' && levelOfBrackets == 0 {
-//                 brackets = false;
-//                 calculate_derivative(buffor.clone());
-//                 buffor.clear();
-//                 continue;
-//             }
-//             if c == ')' {
-//                 levelOfBrackets -= 1;
-//             }
-//             if c == '(' {
-//                 levelOfBrackets += 1;
-//             }
-//             buffor.push(c);
-//         }
-//         if c == '(' {
-//             brackets = true;
-//         }
-//         if c == ')' {
-//             println!("Error: Unopened bracket");
-//             std::process::exit(1);
-//         }
-
-//         if c.is_digit(10) {
-//             numberBuffor.push(c);
-//         }
-//         if c == '+' || c == '-'  {
-//             if !numberBuffor.is_empty() {
-//                 if buffor.is_empty() {
-//                     // 0
-//                 } else {
-//                     calculate_derivative(buffor);
-//                     buffor.clear();
-//                 }
-//                 numberBuffor.clear();
-//             }
-//             derivative.push(c);
-//         }
-//     }
-// }
-fn simple_expression(num: i32, op: Operator, variable: String, power: Option<i32>) -> String{
-    match op {
-        Operator::Pow => {
-            if power.unwrap() == 2 {
-                return format!("*{}{}", num*2, variable);
+fn split_expression(expression: String) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let mut temp = String::new();
+    for c in expression.chars() {
+        if c == '+' || c == '-' || c == '(' || c== ')' {
+            if !temp.is_empty() {
+                result.push(temp.clone());
+                temp.clear();
             }
-            return format!("*{}{}^{}", num*power.unwrap(), variable, power.unwrap() - 1);
-        }
-        Operator::Sin => {
-            println!("*{}cos({})", num, variable);
-        }
-        Operator::Cos => {
-            println!("*{}sin({})", num, variable);
-        }
-        Operator::Tan => {
-            println!("*{}sec^2({})", num, variable);
-        }
-        Operator::Log => {
-            println!("*{}/{}ln(10)", num, variable);
-        }
-        Operator::Ln => {
-            println!("{}/{}", num, variable);
-        }
-        Operator::Sqrt => {
-            println!("*{}/(2*sqrt({}))", num, variable);
-        }
-        Operator::Exp => {
-            println!("*{}exp({})", num, variable);
+            result.push(c.to_string());
+        } else {
+            temp.push(c);
         }
     }
-    return String::new();
+    if !temp.is_empty() {
+        result.push(temp.clone());
+    }
+    result
+}
+fn calculate_simple_derivative(expression: String) -> String {
+    let mut result = String::new();
+    let mut cooficient = String::new();
+    let mut power = String::new();
+    let mut operator = Operator::Pow;
+    let mut seen_operator = false;
+
+    let mut chain_rule = String::new();
+    for c in expression.chars(){
+        if !seen_operator{
+            if c.is_numeric(){
+                cooficient.push(c);
+            }else if c == 'x' {
+                continue;
+            }else{
+                seen_operator = true;
+                match c {
+                    'p' => operator = Operator::Pow,
+                    's' => operator = Operator::Sin,
+                    'c' => operator = Operator::Cos,
+                    't' => operator = Operator::Tan,
+                    'l' => operator = Operator::Log,
+                    'n' => operator = Operator::Ln,
+                    'r' => operator = Operator::Sqrt,
+                    'e' => operator = Operator::Exp,
+                    _ => {}
+                }
+            
+            }
+        }
+        else{
+            if operator == Operator::Pow {
+                power.push(c);
+            }else{
+                chain_rule.push(c);
+            }
+        }
+    }
+    println!("Cooficient: {}, power: {}", cooficient, power);
+    let mut new_cooficient = 1;
+    if !cooficient.is_empty() {
+        new_cooficient = cooficient.parse::<i32>().unwrap();
+    }
+    match operator {
+        
+        Operator::Pow => {
+            let new_power = power.parse::<i32>().unwrap() - 1;
+
+            result.push_str(&(new_cooficient*(new_power+1)).to_string());
+            if new_power == 1 {
+                result.push_str("x");
+            }else{
+                result.push_str("x^");
+                result.push_str(&new_power.to_string());
+            }
+        }
+        Operator::Sin => {
+            result.push_str(&cooficient);
+            result.push_str("cos(");
+            result.push_str(&chain_rule);
+            result.push_str(")");
+        }
+        Operator::Cos => {
+            result.push_str("-");
+            result.push_str(&cooficient);
+            result.push_str("sin(");
+            result.push_str(&chain_rule);
+            result.push_str(")");
+        }
+        Operator::Tan => {
+            result.push_str(&cooficient);
+            result.push_str("sec^2(");
+            result.push_str(&chain_rule);
+            result.push_str(")");
+        }
+        Operator::Log => {
+            result.push_str(&cooficient);
+            result.push_str("1/");
+            result.push_str(&chain_rule);
+        }
+        Operator::Ln => {
+            result.push_str(&cooficient);
+            result.push_str("1/");
+            result.push_str(&chain_rule);
+        }
+        Operator::Sqrt => {
+            result.push_str(&cooficient);
+            result.push_str("1/(2*sqrt(");
+            result.push_str(&chain_rule);
+            result.push_str(")");
+        }
+        Operator::Exp => {
+            result.push_str(&cooficient);
+            result.push_str("exp(");
+            result.push_str(&chain_rule);
+            result.push_str(")");
+        }
+    }
+    result
 }
